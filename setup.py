a42d9a72510a79c9397b12ada77f232a8013816f
DWAA1660 <dwatnip123@gmail.com>
2023-06-03 16:28:45 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='polywrapper',
-    version='0.1.2',
+    version='0.1.3',
     author='David',
     author_email='dwatnip123@gmail.com',
     description='A wrapper for interacting with Polygon DB',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=['polywrapper'],
     install_requires=[
         'websocket-client',
