0f8ec40559f93b89700291d1ce4f2ab6ee4ee7ba
JewishLewish <lanbrokhman@gmail.com>
2023-10-14 11:15:37 -0700

    Cleanup Code

--- Polyutils/src/lib.rs
+++ Polyutils/src/lib.rs
@@ -1,46 +1,48 @@
-use std::{process::{Stdio, Command}, io::{Write, Read}};
+use std::{process::{Stdio, Command, Child}, io::{Write, Read}};
 
-use pyo3::prelude::*;
+use pyo3::{prelude::*, types::PyList, PyNativeType};
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn execute(input: String) -> PyResult<String> {
-    // Command to run a program (e.g., "echo" in this example)
-    let mut cmd = Command::new("./polygondb.exe");
+#[pyclass]
+struct Polygon {
+    exechh: Child
+}
 
-    // Set up stdin, stdout, and stderr
+
+#[pymethods]
+impl Polygon {
+    #[new]
+    fn new(targetfile: String) -> Self {
+        
+        Polygon { exechh: setup(targetfile) }
+    }
+
+    fn execute(&mut self, input: String) -> PyResult<String>{
+        if let Some(mut stdin) = self.exechh.stdin.take() {
+            stdin.write_all(input.as_bytes()).expect("Failed to write to stdin");
+        }
+    
+        let mut output = String::new();
+    
+        // Read data from the program's stdout
+        if let Some(mut stdout) = self.exechh.stdout.take() {
+            output = String::new();
+            stdout.read_to_string(&mut output).expect("Failed to read from stdout");
+        }
+        Ok(output)
+    }
+}
+
+fn setup (t: String) -> Child {
+    let mut cmd = Command::new(t);
     cmd.stdin(Stdio::piped());
     cmd.stdout(Stdio::piped());
-
-    // Start the command
-    let mut child = cmd.spawn().expect("Failed to start command");
-
-    // Write data to the program's stdin
-    if let Some(mut stdin) = child.stdin.take() {
-        stdin.write_all(input.as_bytes()).expect("Failed to write to stdin");
-    }
-
-    let mut output = String::new();
-
-    // Read data from the program's stdout
-    if let Some(mut stdout) = child.stdout.take() {
-        output = String::new();
-        stdout.read_to_string(&mut output).expect("Failed to read from stdout");
-        println!("Program output: {}", output);
-    }
-
-    // Wait for the command to finish
-    let status = child.wait().expect("Failed to wait for command");
-    if status.success() {
-        Ok(output)
-    } else {
-        Ok("Fail".to_string())
-    }
+    cmd.stderr(Stdio::null());
+    return cmd.spawn().expect("Failed to start command");
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn Polyutils(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(execute, m)?)?;
+    m.add_class::<Polygon>()?;
     Ok(())
 }
--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-import sys
-from timeit import timeit
-
+import timeit
+import json
 import Polyutils
 
-def main():
-    Polyutils.execute("test")
+def benchmark(func, num_runs=1):
+    total_time = timeit.timeit(func, number=num_runs)
+    print(f"\nAverage execution time over {num_runs} runs: {total_time / num_runs:.6f} seconds")
 
+test = Polyutils.Polygon("./polygondb.exe")
 
+def use_polygon():
+   return test.test("test")
+
+def use_json():
+  with open('databases/bigdata.json') as f:
+    return json.load(f)
+   
 if __name__ == '__main__':
-    
-    main()
+    benchmark(use_polygon, num_runs=90)
+    benchmark(use_json, num_runs=90)
