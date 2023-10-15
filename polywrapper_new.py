bc5c2d986986d60eaa1a3f35b2317e45e08d4a25
JewishLewish <lanbrokhman@gmail.com>
2023-10-14 17:29:00 -0700

    CRUD

--- polywrapper_new.py
+++ polywrapper_new.py
@@ -24,8 +24,10 @@ class PolyClient:
       Retrieve data from the database.
 
       Args:
-          key (str, optional): The key or location to retrieve data from.
-              Defaults to an empty string, which retrieves the entire database.
+          dbname (str, required): Name of Database
+          
+          location (str,optional): Location in Data
+              Defaults to an empty string, which retrieves the entire database
 
       Returns:
           dict: The retrieved data as a dictionary.
@@ -36,6 +38,89 @@ class PolyClient:
                   "dbname": dbname,
                   "location": location,
                   "action": "read",
+                  "value" : 0
+              }
+          )
+      )
+      return json.loads(self.ws.recv())
+
+    def create(self, dbname: str, location: str = "", value = None):
+      """
+      Creates data from the database.
+
+      Args:
+          dbname (str, required): Name of Database
+          
+          location (str,optional): Location in Data
+              Defaults to an empty string, which retrieves the entire database
+
+      Returns:
+          dict: The retrieved data as a dictionary.
+      """
+
+      if value == None:
+        return json.loads("""{"Status":"1", "Message":"Please Assign Value to a Variable"}""")
+      
+      self.ws.send(
+          json.dumps(
+              {
+                  "dbname": dbname,
+                  "location": location,
+                  "action": "create",
+                  "value" : value
+              }
+          )
+      )
+      return json.loads(self.ws.recv())
+
+    def modify(self, dbname: str, location: str = "", value = None):
+      """
+      Modifies data from the database.
+
+      Args:
+          dbname (str, required): Name of Database
+          
+          location (str,optional): Location in Data
+              Defaults to an empty string, which retrieves the entire database
+
+      Returns:
+          dict: The retrieved data as a dictionary.
+      """
+
+      if value == None:
+        return json.loads("""{"Status":"1", "Message":"Please Assign Value to a Variable"}""")
+      
+      self.ws.send(
+          json.dumps(
+              {
+                  "dbname": dbname,
+                  "location": location,
+                  "action": "modify",
+                  "value" : value
+              }
+          )
+      )
+      return json.loads(self.ws.recv())
+
+    def delete(self, dbname: str, location: str = ""):
+      """
+      Deletes data from the database.
+
+      Args:
+          dbname (str, required): Name of Database
+          
+          location (str,optional): Location in Data
+              Defaults to an empty string, which retrieves the entire database
+
+      Returns:
+          dict: The retrieved data as a dictionary.
+      """
+      self.ws.send(
+          json.dumps(
+              {
+                  "dbname": dbname,
+                  "location": location,
+                  "action": "delete",
                   "value" : 0
               }
           )
